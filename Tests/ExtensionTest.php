8f3e6071e3d97d473810fed86550427d79285f58
lidaa <aa_dil@hotmail.fr>
2012-09-13 14:59:10 +0100

    test

---
+++ Tests/ExtensionTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+* This file is part of the LidaaTransBundle package.
+*/
+
+namespace Lidaa\TransBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Lidaa\TransBundle\DependencyInjection\LidaaTransExtension as TransExtension;
+
+/**
+* TransExtension
+*
+* @author Lidaa <aa_dil@hotmail.fr>
+*/
+class ExtensionTest extends WebTestCase
+{    
+    public function testLoadEmptyConfiguration()
+    {
+    	$container = $this->createContainer();
+    	$container->registerExtension(new TwigExtension());
+    	$container->loadFromExtension('lidaa_trans', array());
+    	$this->compileContainer($container);
+
+    	$this->assertEquals('Lidaa\TransBundle\Config\ConfigBuilder', $container->getParameter('lidaa_trans.config_builder.class'));
+    	$this->assertEquals('Lidaa\TransBundle\Translation\Translation', $container->getParameter('lidaa_trans.translation.class'));
+    }
+    
+    private function createContainer()
+    {
+    	$container = new ContainerBuilder(new ParameterBag(array(
+                'kernel.cache_dir' => __DIR__,
+                'kernel.charset'   => 'UTF-8',
+                'kernel.debug'     => false,
+    	)));
+    
+    	return $container;
+    }
+    
+    private function compileContainer(ContainerBuilder $container)
+    {
+    	$container->getCompilerPassConfig()->setOptimizationPasses(array());
+    	$container->getCompilerPassConfig()->setRemovingPasses(array());
+    	$container->compile();
+    }
+}
+
